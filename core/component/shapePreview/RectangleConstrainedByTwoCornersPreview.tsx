da8ff1f7db6af080421f86a90e4a096e08f2d8a4
inside-hakumai <inside.rice164@gmail.com>
2022-07-30 17:16:00 +0900

    ビルドエラー修正

--- core/component/shapePreview/RectangleConstrainedByTwoCornersPreview.tsx
+++ core/component/shapePreview/RectangleConstrainedByTwoCornersPreview.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface Props {
-  shape: RectangleTwoCornersSeed2
+  shape: RectangleSeedConstrainedByTwoCorners
 }
 
 const RectangleConstrainedByTwoCornersPreview: React.FC<Props> = ({ shape }) => {
