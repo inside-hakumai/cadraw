330376c802718a125a8336e11b7dcf47b6c0c4e0
inside-hakumai <inside.rice164@gmail.com>
2022-08-03 01:12:18 +0900

    選択処理をカスタムフックに切り出し

---
+++ core/container/hooks/useSelectOperation.ts
@@ -0,0 +1,22 @@
+import { useRecoilCallback } from 'recoil'
+import { indicatingShapeIdState, selectedShapeIdsState } from '../states'
+
+const useSelectOperation = () => {
+  const triggerSelectOperation = useRecoilCallback(({ snapshot, set }) => async () => {
+    const indicatingShapeId = await snapshot.getPromise(indicatingShapeIdState)
+
+    if (indicatingShapeId !== null) {
+      set(selectedShapeIdsState, oldValue => {
+        if (oldValue.includes(indicatingShapeId)) {
+          return oldValue.filter(id => id !== indicatingShapeId)
+        } else {
+          return [...oldValue, indicatingShapeId]
+        }
+      })
+    }
+  })
+
+  return { triggerSelectOperation }
+}
+
+export default useSelectOperation
